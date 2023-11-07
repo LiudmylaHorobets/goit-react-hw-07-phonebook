276a09ad6be4fb636cc78ce4984ba6e7385e77bc
LiudmylaHorobets <horobets1323@gmail.com>
2023-11-07 15:18:22 +0200

    fix pattern

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -43,23 +43,27 @@ export const ContactForm = () => {
       <label className={css.label}>
         <p className={css.labelTitle}>Name</p>
         <input
+          className={css.formInput}
           type="text"
           name="name"
           placeholder="Rosie Simpson"
-          title="Name may contain only letters, apostrophe, dash and spaces"
+          pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
-          className={css.formInput}
+          id="nameId"
         />
       </label>
       <label className={css.label}>
         <p className={css.labelTitle}>Number</p>
         <input
+          className={css.formInput}
           type="tel"
           name="number"
           placeholder="459-12-56"
+          pattern="\+?\d{1,4}?[\-.\s]?\(?\d{1,3}?\)?[\-.\s]?\d{1,4}[\-.\s]?\d{1,4}[\-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
-          className={css.formInput}
+          id="numberId"
         />
       </label>
       <button className={css.buttonForm}>Add contact</button>
